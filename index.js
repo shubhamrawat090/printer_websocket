394a610ab65180ca34c6571bf590b5be92806288
Grudge69 <shubham.shubham.rawat090@gmail.com>
2023-05-30 13:00:41 +0530

    socket code completed

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const io = require('socket.io-client');
-const axios = require('axios');
-const fs = require('fs');
-const { print } = require('pdf-to-printer');
+const io = require("socket.io-client");
+const axios = require("axios");
+const fs = require("fs");
+const { print } = require("pdf-to-printer");
 
-const socket = io('https://samasya.tech');
+const socket = io("https://samasya.tech");
 
-socket.on('connect', () => {
-  console.log('Connected to backend socket');
-  
+socket.on("connect", () => {
+  console.log("Connected to backend socket");
+
   // Join the "printer_system" room
-  socket.emit('printer_socket_room', 'printer_system');
+  socket.emit("printer_socket_room", "printer_system");
 });
 
-socket.on('get_printer_system', async (pdfUrl) => {
+socket.on("get_printer_system", async (pdfUrl) => {
   try {
-    
     // Download the PDF from pdfUrl
-    const response = await axios.get(pdfUrl, { responseType: 'arraybuffer' });
+    const response = await axios.get(pdfUrl, { responseType: "arraybuffer" });
     const pdfBuffer = response.data;
 
     // Save the PDF to a temporary file
-    const tempFilePath = './temp.pdf';
+    const tempFilePath = "./temp.pdf";
     fs.writeFileSync(tempFilePath, pdfBuffer);
 
     // Print the PDF
@@ -29,12 +28,12 @@ socket.on('get_printer_system', async (pdfUrl) => {
     // Delete the temporary file
     fs.unlinkSync(tempFilePath);
 
-    console.log('PDF printed successfully');
+    console.log("PDF printed successfully");
   } catch (error) {
-    console.error('Failed to print PDF: ', error);
+    console.error("Failed to print PDF: ", error);
   }
 });
 
-socket.on('disconnect', () => {
-  console.log('Disconnected from backend socket');
+socket.on("disconnect", () => {
+  console.log("Disconnected from backend socket");
 });
