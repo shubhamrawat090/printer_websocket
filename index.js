00f3138b4f1c79f2caee1fd5c1e336c2d192ebe3
Grudge69 <shubham.shubham.rawat090@gmail.com>
2023-05-30 12:49:25 +0530

    changes

--- index.js
+++ index.js
@@ -1,72 +1,40 @@
-const socket = require("socket.io");
-const express = require("express");
-const cors = require("cors");
-const { print } = require('pdf-to-printer');
+const io = require('socket.io-client');
 const axios = require('axios');
 const fs = require('fs');
-const path = require('path');
+const { print } = require('pdf-to-printer');
 
-const app = express();
-app.use(cors({ origin: "*" }));
-app.use(express.json());
+const socket = io('https://samasya.tech');
 
-const os = require('os');
-
-function getIPAddress() {
-  const interfaces = os.networkInterfaces();
-  let ipAddress;
-
-  for (const networkInterface of Object.values(interfaces)) {
-    const found = networkInterface.find(
-      (details) => details.family === 'IPv4' && !details.internal
-    );
-
-    if (found) {
-      ipAddress = found.address;
-      break;
-    }
-  }
-
-  return ipAddress;
-}
-
-const PORT = 8080;
-const IP_ADDRESS = getIPAddress();
-
-const server = app.listen(PORT, IP_ADDRESS, () => {
-  console.log(`Server connected at PORT: ${PORT}`);
-});
-
-const io = socket(server);
-
-io.sockets.on("connection", (socket) => {
-  console.log("new connection id: ", socket.id);
-  sendData(socket);
-});
-
-async function sendData(socket) {
-  socket.on("print_data", async (data) => {
-    try {
-      await printPdfFromUrl(data);
-      console.log('PDF printed successfully');
-    } catch (error) {
-      console.error('Error printing PDF:', error);
-    }
-  });
-}
-
-async function printPdfFromUrl(pdfUrl) {
-  const response = await axios.get(pdfUrl, { responseType: 'arraybuffer' });
-  if (!response.status === 200) {
-    throw new Error(`Failed to download PDF: ${response.status} ${response.statusText}`);
-  }
-
-  const pdfBuffer = response.data;
-  const tempFilePath = path.join(__dirname, 'temp.pdf');
-
-  fs.writeFileSync(tempFilePath, pdfBuffer);
-
-  await print(tempFilePath);
+socket.on('connect', () => {
+  console.log('Connected to backend socket');
   
-  fs.unlinkSync(tempFilePath);
-}
+  // Join the "printer_system" room
+  socket.emit('printer_socket_room', 'printer_system');
+});
+
+socket.on('get_printer_system', async (pdfUrl) => {
+  try {
+    
+    // Download the PDF from pdfUrl
+    const response = await axios.get(pdfUrl, { responseType: 'arraybuffer' });
+    const pdfBuffer = response.data;
+
+    // Save the PDF to a temporary file
+    const tempFilePath = './temp.pdf';
+    fs.writeFileSync(tempFilePath, pdfBuffer);
+
+    // Print the PDF
+    await print(tempFilePath);
+
+    // Delete the temporary file
+    fs.unlinkSync(tempFilePath);
+
+    console.log('PDF printed successfully');
+  } catch (error) {
+    console.error('Failed to print PDF: ', error);
+  }
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from backend socket');
+});
